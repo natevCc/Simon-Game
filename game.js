9e7bcabb785f7b8630ee8b20ebcea27d505c7bf6
Цветан Цветанов Василев <cvvasilev@tu-sofia.bg>
2023-09-09 17:48:18 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,81 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var isStarted = false;
+var level = 0;
+var nextSequenceTimeout;
+
+$(document).keypress(function() {
+    if (isStarted) {
+        return;
+    }
+    isStarted = true;
+    nextSequence();
+});
+
+$(".btn").click(function(evt) {
+    if (!isStarted) {
+        return;
+    }
+
+    var userChosenColour = evt.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(audioName) {
+    var audio = new Audio(`./sounds/${audioName}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    var button = $(`#${currentColour}`);
+    button.addClass("pressed");
+    setTimeout(() => {
+        button.removeClass("pressed");
+    }, 100);
+}
+
+function nextSequence() {
+    var randomNumber = Math.round(Math.random() * 3);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    userClickedPattern = [];
+
+    level++;
+
+    updateTitle(`Ниво ${level}`);
+}
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            nextSequenceTimeout = setTimeout(nextSequence, 1000);
+        }
+    } else {
+        clearTimeout(nextSequenceTimeout);
+        playSound("wrong");
+        updateTitle("Ти загуби, Натисни клавиш за Рестарт");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        startOver();
+    }
+}
+
+function startOver() {
+    isStarted = false;
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+}
+
+function updateTitle(text) {
+    $("#level-title").text(text);
+}
